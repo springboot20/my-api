5478b0065f2e39c76d68de91db7853168d3ce2c5
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-05-30 17:24:13 +0100

    file modified

--- model/TransactionSchema.js
+++ model/TransactionSchema.js
@@ -7,7 +7,7 @@ const TransactionSchema = new Schema({
     require: [true, "Please provide a user"],
   },
   receiverId: {
-    type: schema.Types.ObjectId,
+    type: Schema.Types.ObjectId,
     ref: "User",
     require: [true, "Please provide a user"],
   },
