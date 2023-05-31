22af7e1581cd9c6b7103f42d3dcbf151f6628ab6
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-05-31 08:54:00 +0100

    file changes

--- index.js
+++ index.js
@@ -53,5 +53,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server running at port : http://localhost:${procees.env.PORT}`);
+  console.log(`Server running at port : http://localhost:${process.env.PORT}`);
 });
