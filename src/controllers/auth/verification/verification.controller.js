984ef6faa971a5a3847af0b516a4ccb27e6c9462
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2024-08-07 15:58:10 +0100

    reset password and reset-password request email controller error fixed and up an running

--- src/controllers/auth/verification/verification.controller.js
+++ src/controllers/auth/verification/verification.controller.js
@@ -1,9 +1,7 @@
-import crypto from "crypto";
 import {
   ApiResponse,
   apiResponseHandler,
 } from "../../../middleware/api/api.response.middleware.js";
-import { mongooseTransactions } from "../../../middleware/mongoose/mongoose.transactions.js";
 import { UserModel } from "../../../models/index.js";
 import { CustomErrors } from "../../../middleware/custom/custom.errors.js";
 import { validateToken } from "../../../utils/jwt.js";
@@ -12,125 +10,124 @@ import { generateTokens } from "../login/login.controller.js";
 import bcrypt from "bcrypt";
 import { sendMail } from "../../../service/email.service.js";
 
-export const resetPassword = apiResponseHandler(
-  mongooseTransactions(async (req, res) => {
-    const { resetToken } = req.params;
-    const { newPassword } = req.body;
+export const resetPassword = apiResponseHandler(async (req, res) => {
+  const { resetToken } = req.params;
+  const { password } = req.body;
 
-    if (!resetToken)
+  if (!resetToken)
+    throw new CustomErrors(
+      "Reset password token is missing",
+      StatusCodes.NOT_FOUND,
+    );
+
+  const user = await UserModel.findOne({
+    _id: req.user.id,
+    forgotPasswordTokenExpiry: { $gte: Date.now() },
+  });
+
+  if (!user)
+    throw new CustomErrors(
+      "Token is invalid or expired",
+      StatusCodes.UNAUTHORIZED,
+    );
+
+  const validToken = await bcrypt.compare(resetToken, user.forgotPasswordToken);
+
+  if (!validToken)
+    throw new CustomErrors(
+      "Invalid reset password token provided",
+      StatusCodes.UNAUTHORIZED,
+    );
+
+  user.forgotPasswordToken = undefined;
+  user.forgotPasswordTokenExpiry = undefined;
+  user.password = password;
+
+  await user.save({ validateBeforeSave: false });
+
+  return new ApiResponse(StatusCodes.OK, {}, "Password reset successfully");
+});
+
+export const changeCurrentPassword = apiResponseHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  const user = await UserModel.findById(req.user?._id);
+
+  if (!(await user.matchPasswords(oldPassword)))
+    throw new CustomErrors("Invalid old password", StatusCodes.BAD_REQUEST);
+
+  user.password = newPassword;
+  user.save({ validateBeforeSave: false });
+
+  return new ApiResponse(StatusCodes.OK, {}, "Password changed successfully");
+});
+
+export const forgotPassword = apiResponseHandler(async (req, res) => {
+  const { email } = req.body;
+
+  const user = await UserModel.findOne({ email });
+
+  if (!user) throw new CustomErrors("User not found", StatusCodes.NOT_FOUND);
+
+  const { unHashedToken, hashedToken, tokenExpiry } =
+    await user.generateTemporaryTokens();
+
+  user.forgotPasswordToken = hashedToken;
+  user.forgotPasswordTokenExpiry = tokenExpiry;
+  await user.save({ validateBeforeSave: false });
+
+  // const resetLink = `${process.env.BASE_URL}/reset-password/${unHashedToken}`;
+  const resetLink = `${req.protocol}//:${req.get(
+    "host",
+  )}/api/v1/users/reset-password/${unHashedToken}`;
+
+  await sendMail(
+    user.email,
+    "Password reset",
+    { resetLink, username: user.username },
+    "reset",
+  );
+
+  return new ApiResponse(
+    StatusCodes.OK,
+    { unHashedToken },
+    "Password reset link sent to your email",
+  );
+});
+
+export const refreshToken = apiResponseHandler(async (req, res, session) => {
+  const {
+    body: { inComingRefreshToken },
+  } = req;
+
+  try {
+    const decodedRefreshToken = validateToken(inComingRefreshToken);
+    let user = await UserModel.findByIdAndUpdate(decodedRefreshToken?._id);
+
+    if (!user) {
+      throw new CustomErrors("Invalid Token", StatusCodes.UNAUTHORIZED);
+    }
+
+    if (inComingRefreshToken !== user?.refreshToken) {
       throw new CustomErrors(
-        "Reset password token is missing",
-        StatusCodes.BAD_REQUEST,
-      );
-
-    let hashedToken = crypto
-      .createHash("sha256")
-      .update(resetToken)
-      .digest("hex");
-
-    const user = await UserModel.findOne({
-      forgotPasswordToken: hashedToken,
-      forgotPasswordTokenExpiry: { $gt: Date.now() },
-    });
-
-    if (!user)
-      throw new CustomErrors(
-        "Token is invalid or expired",
+        "Token has expired or has already been used",
         StatusCodes.UNAUTHORIZED,
       );
-
-    user.emailVerificationToken = undefined;
-    user.emailVerificationTokenExpiry = undefined;
-
-    user.password = newPassword;
-    await user.save({ validateBeforeSave: false });
-
-    return new ApiResponse(StatusCodes.OK, {}, "Password reset successfully");
-  }),
-);
-
-export const changeCurrentPassword = apiResponseHandler(
-  mongooseTransactions(async (req, res) => {
-    const { oldPassword, newPassword } = req.body;
-
-    const user = await UserModel.findById(req.user?._id);
-
-    if (!(await user.matchPasswords(oldPassword)))
-      throw new CustomErrors("Invalid old password", StatusCodes.BAD_REQUEST);
-
-    user.password = newPassword;
-    user.save({ validateBeforeSave: false });
-
-    return new ApiResponse(StatusCodes.OK, {}, "Password changed successfully");
-  }),
-);
-
-export const forgotPassword = apiResponseHandler(
-  mongooseTransactions(async (req, res, session) => {
-    const { email } = req.body;
-
-    const user = await UserModel.findOne({ email });
-    if (!user) throw new CustomErrors("User not found", StatusCodes.NOT_FOUND);
-
-    const { unHashedToken, hashedToken, tokenExpiry } =
-      await user.generateTemporaryTokens();
-
-    user.forgotPasswordToken = hashedToken;
-    user.forgotPasswordTokenExpiry = new Date(tokenExpiry);
-    await user.save({ validateBeforeSave: false });
-
-    const resetLink = `${process.env.BASE_URL}/reset-password/${unHashedToken}`;
-    await sendMail(
-      user.email,
-      "Password reset",
-      { resetLink, username: user.username },
-      "reset-password",
-    );
-
-    await user.save({ validateBeforeSave: false, session });
-    return new ApiResponse(
-      StatusCodes.OK,
-      {},
-      "Password reset link sent to your email",
-    );
-  }),
-);
-
-export const refreshToken = apiResponseHandler(
-  mongooseTransactions(async (req, res, session) => {
-    const {
-      body: { inComingRefreshToken },
-    } = req;
-
-    try {
-      const decodedRefreshToken = validateToken(inComingRefreshToken);
-      let user = await UserModel.findByIdAndUpdate(decodedRefreshToken?._id);
-
-      if (!user) {
-        throw new CustomErrors("Invalid Token", StatusCodes.UNAUTHORIZED);
-      }
-
-      if (inComingRefreshToken !== user?.refreshToken) {
-        throw new CustomErrors(
-          "Token has expired or has already been used",
-          StatusCodes.UNAUTHORIZED,
-        );
-      }
-
-      const { accessToken, refreshToken } = await generateTokens(user?._id);
-
-      user.refreshToken = refreshToken;
-      await user.save({ session });
-
-      return {
-        message: "access token refreshed successfully",
-        tokens: { accessToken, refreshToken },
-      };
-    } catch (error) {
-      return new CustomErrors(`Error : ${error}`, StatusCodes.UNAUTHORIZED);
     }
-  }),
-);
+
+    const { accessToken, refreshToken } = await generateTokens(user?._id);
+
+    user.refreshToken = refreshToken;
+    await user.save({ session });
+
+    return {
+      message: "access token refreshed successfully",
+      tokens: { accessToken, refreshToken },
+    };
+  } catch (error) {
+    return new CustomErrors(`Error : ${error}`, StatusCodes.UNAUTHORIZED);
+  }
+});
 
 export const verifyEmail = apiResponseHandler(async (req, res) => {
   const { verificationToken, userId } = req.params;
